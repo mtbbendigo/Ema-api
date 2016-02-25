e55c02f231304f184dd3122e9d5df8625b8821d2
Gazzman <gareth.baker@bendigoadelaide.com.au>
2016-02-26 10:06:04 +1100

    Remove printlns and cleanup ready for dev deployment

--- api/helpers/mssql-datasource.js
+++ api/helpers/mssql-datasource.js
@@ -31,8 +31,8 @@ function extractLogsParamsFromRequest(req, callback)
     var appss = (req.swagger.params.apps.value) ? req.swagger.params.apps.value:null;
     var reqMess = (req.swagger.params.requestMessage.value) ? req.swagger.params.requestMessage.value:null;
     var logMsg = (req.swagger.params.logMessage.value) ? req.swagger.params.logMessage.value:null;
-    var errs = (req.swagger.params.errorsOnly.value) ? req.swagger.params.errorsOnly.value:null;
-    var ping = (req.swagger.params.includeOlbPing.value) ? req.swagger.params.includeOlbPing.value:1;
+    var errs = (req.swagger.params.errorsOnly.value) ? req.swagger.params.errorsOnly.value:0;
+    var ping = (req.swagger.params.includeOlbPing.value) ? req.swagger.params.includeOlbPing.value:0;
 
     var logsRequest = {
         startIndex: startPos,
